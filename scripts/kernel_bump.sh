885f95a7739952f837449ba91256eb486d5bd888
Elliott Mitchell <ehem+openwrt@m5p.com>
2024-03-19 20:43:48 -0700

    scripts/kernel_bump: turn kernel_bump.sh into wrapper around kernel_upgrade.pl
    Demonstrating kernel_upgrade.pl's superior functionality.
    
    Signed-off-by: Elliott Mitchell <ehem+openwrt@m5p.com>

--- scripts/kernel_bump.sh
+++ scripts/kernel_bump.sh
@@ -4,25 +4,6 @@
 # Copyright (C) 2024 Olliver Schinagl <oliver@schinagl.nl>
 
 set -eu
-if [ -n "${DEBUG_TRACE_SH:-}" ] && \
-   [ "${DEBUG_TRACE_SH:-}" != "${DEBUG_TRACE_SH#*"$(basename "${0}")"*}" ] || \
-   [ "${DEBUG_TRACE_SH:-}" = 'all' ]; then
-	set -x
-fi
-
-REQUIRED_COMMANDS='
-	[
-	basename
-	command
-	echo
-	exit
-	git
-	printf
-	sed
-	set
-	shift
-	sort
-'
 
 _msg()
 {
@@ -42,16 +23,6 @@ e_err()
 	_msg 'err' "${*}" >&2
 }
 
-e_warn()
-{
-	_msg 'warning' "${*}"
-}
-
-e_notice()
-{
-	_msg 'notice' "${*}"
-}
-
 usage()
 {
 	echo "Usage: ${0}"
@@ -65,130 +36,6 @@ usage()
 	echo 'All options can also be passed in environment variables (listed between [BRACKETS]).'
 	echo 'Note that this script must be run from within the OpenWrt git repository.'
 	echo 'Example: scripts/kernel_bump.sh -p realtek -s v6.1 -t v6.6'
-}
-
-cleanup()
-{
-	trap - EXIT HUP INT QUIT ABRT ALRM TERM
-
-	if [ -n "${initial_branch:-}" ] && \
-	   [ "$(git rev-parse --abbrev-ref HEAD)" != "${initial_branch:-}" ]; then
-		git switch "${initial_branch}"
-	fi
-}
-
-init()
-{
-	src_file="$(readlink -f "${0}")"
-	src_dir="${src_file%%"${src_file##*'/'}"}"
-	initial_branch="$(git rev-parse --abbrev-ref HEAD)"
-	initial_commitish="$(git rev-parse HEAD)"
-
-	if [ -n "$(git status --porcelain | grep -v '^?? .*')" ]; then
-		echo 'Git respository not in a clean state, will not continue.'
-		exit 1
-	fi
-
-	if [ -n "${src_dir##*'/scripts/'}" ]; then
-		echo "This script '${src_file}' is not in the scripts subdirectory, this is unexpected, cannot continue."
-		exit 1
-	fi
-
-	source_version="${source_version#v}"
-	target_version="${target_version#v}"
-
-	trap cleanup EXIT HUP INT QUIT ABRT ALRM TERM
-}
-
-bump_kernel()
-{
-	git switch --force-create '__openwrt_kernel_files_mover'
-
-	if [ "${config_only:-false}" != 'true' ]; then
-		for _path in $(git ls-tree -d -r --name-only '__openwrt_kernel_files_mover' "${_target_dir}" |
-			       sed -n "s|^\(.*-${source_version}\).*|\1|p" |
-			       sort -u); do
-			if [ ! -e "${_path}" ] || \
-			   [ "${_path}" = "${_path%%"-${source_version}"}" ]; then
-				continue
-			fi
-
-			_target_path="${_path%%"-${source_version}"}-${target_version}"
-			if [ -e "${_target_path}" ]; then
-				e_err "Target '${_target_path}' already exists!"
-				exit 1
-			fi
-
-			git mv \
-				"${_path}" \
-				"${_target_path}"
-		done
-	fi
-
-	for _config in $(git ls-files "${_target_dir}" |
-	                 sed -n "s|^\(.*config-${source_version}\).*|\1|p" |
-	                 sort -u); do
-		if [ ! -e "${_config}" ]; then
-			continue
-		fi
-
-		_subtarget="${_config%%"/config-${source_version}"}"
-		if [ -n "${subtarget_names:-}" ]; then
-			echo "${subtarget_names:-}" | while IFS=',' read -r _subtarget_name; do
-				if [ "${_subtarget_name}" = "${_subtarget##*'/'}" ]; then
-					git mv "${_config}" "${_subtarget}/config-${target_version}"
-				fi
-			done
-		else
-			git mv "${_config}" "${_subtarget}/config-${target_version}"
-		fi
-	done
-
-	git commit \
-		--signoff \
-		--message "kernel/${platform_name}: Create kernel files for v${target_version} (from v${source_version})" \
-		--message 'This is an automatically generated commit.' \
-		--message 'When doing `git bisect`, consider `git bisect --skip`.'
-
-	git checkout 'HEAD~' "${_target_dir}"
-	git commit \
-		--signoff \
-		--message "kernel/${platform_name}: Restore kernel files for v${source_version}" \
-		--message "$(printf "This is an automatically generated commit which aids following Kernel patch\nhistory, as git will see the move and copy as a rename thus defeating the\npurpose.\n\nFor the original discussion see:\nhttps://lists.openwrt.org/pipermail/openwrt-devel/2023-October/041673.html")"
-	git switch "${initial_branch:?Unable to switch back to original branch. Quitting.}"
-	GIT_EDITOR=true git merge --no-ff '__openwrt_kernel_files_mover'
-	git branch --delete '__openwrt_kernel_files_mover'
-	echo "Deleting merge commit ($(git rev-parse HEAD))."
-	git rebase HEAD~1
-
-	echo "Original commitish was '${initial_commitish}'."
-	echo 'Kernel bump complete. Remember to use `git log --follow`.'
-}
-
-check_requirements()
-{
-	for _cmd in ${REQUIRED_COMMANDS}; do
-		if ! _test_result="$(command -V "${_cmd}")"; then
-			_test_result_fail="${_test_result_fail:-}${_test_result}\n"
-		else
-			_test_result_pass="${_test_result_pass:-}${_test_result}\n"
-		fi
-	done
-
-	echo 'Available commands:'
-	# As the results contain \n, we expect these to be interpreted.
-	# shellcheck disable=SC2059
-	printf "${_test_result_pass:-none\n}"
-	echo
-	echo 'Missing commands:'
-	# shellcheck disable=SC2059
-	printf "${_test_result_fail:-none\n}"
-	echo
-
-	if [ -n "${_test_result_fail:-}" ]; then
-		echo 'Command test failed, missing programs.'
-		test_failed=1
-	fi
 }
 
 main()
@@ -251,11 +98,10 @@ main()
 		exit 1
 	fi
 
-	check_requirements
+	source_version="-$source_version"
+	target_version="-$target_version"
 
-	init
-	bump_kernel
-	cleanup
+	exec "${0%${0##*/}}kernel_upgrade.pl" "$source_version" "$target_version" "$platform_name"
 }
 
 main "${@}"
